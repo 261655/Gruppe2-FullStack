e12b02f3ce4ca553349c884d405e1e0466f99ebf
261655 <261655@usn.no>
2025-05-21 17:17:18 +0200

    Deloppgave 2.4 fullført. Lagt til fargekode for kolonner

--- Gruppe2/Controllers/PollenAPIController.cs
+++ Gruppe2/Controllers/PollenAPIController.cs
@@ -1,16 +1,13 @@
-
-using AspNetCoreGeneratedDocument;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Gruppe2.Models;
 using System.Linq;
-
 
 namespace Gruppe2.Controllers 
 {
     public class PollenAPIController : Controller
     {
         private readonly AppDbContext _context;
-
 
         public PollenAPIController(AppDbContext context)
         {
@@ -20,6 +17,7 @@ namespace Gruppe2.Controllers
         public ActionResult Index()
         {
             var data = _context.IndexInfo
+                               .Include(i => i.ColorInfo) // inkludér fargekoding
                                .OrderByDescending(i => i.ID)
                                .Take(5)
                                .ToList();
